2bdeeb9a30e363888a3f2a4b28eba1cf45a3fd63
mark <mnaeem@neuraco.com>
2025-10-29 11:16:20 +0000

    added rviz2 visualisation and a docker env to run it

---
+++ ros_visualiser/run_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Simple script to run Meta Quest ROS2 visualization in Docker
+# Usage: ./run_docker.sh [IP_ADDRESS] [FREQ] [PORT]
+
+# Build if image doesn't exist
+if [[ "$(docker images -q meta_quest_ros2 2> /dev/null)" == "" ]]; then
+    echo "Building Docker image..."
+    docker build -t meta_quest_ros2 .
+fi
+
+# Setup X11
+xhost +local:docker 2>/dev/null
+
+docker run -it --rm \
+    --privileged \
+    --network host \
+    -e DISPLAY=$DISPLAY \
+    -e PYTHONPATH=/workspace \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $(pwd)/..:/workspace:rw \
+    -v /dev/bus/usb:/dev/bus/usb \
+    meta_quest_ros2 \
+    bash -c "cd /workspace && tmux new-session -d -s meta_quest \
+    'source /opt/ros/rolling/setup.bash && \
+    python3 ros_visualiser/ros2_tf_publisher.py; \
+    bash' \; split-window -h 'source /opt/ros/rolling/setup.bash && \
+    sleep 2 && rviz2 -d ros_visualiser/meta_quest_viewer.rviz; bash' \; attach"
+
