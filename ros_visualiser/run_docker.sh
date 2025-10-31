639b28ae84e034f55bf5379353da4a92b2cf2f5b
mark <mnaeem@neuraco.com>
2025-10-31 17:18:25 +0000

    fixed script directory in run_docker.sh

--- ros_visualiser/run_docker.sh
+++ ros_visualiser/run_docker.sh
@@ -2,10 +2,13 @@
 # Simple script to run Meta Quest ROS2 visualization in Docker
 # Usage: ./run_docker.sh [IP_ADDRESS] [FREQ] [PORT]
 
+# Get the directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Build if image doesn't exist
 if [[ "$(docker images -q meta_quest_ros2 2> /dev/null)" == "" ]]; then
     echo "Building Docker image..."
-    docker build -t meta_quest_ros2 .
+    docker build -t meta_quest_ros2 ${SCRIPT_DIR}
 fi
 
 # Setup X11
@@ -17,7 +20,7 @@ docker run -it --rm \
     -e DISPLAY=$DISPLAY \
     -e PYTHONPATH=/workspace \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v $(pwd)/..:/workspace:rw \
+    -v ${SCRIPT_DIR}/..:/workspace:rw \
     -v /dev/bus/usb:/dev/bus/usb \
     meta_quest_ros2 \
     bash -c "cd /workspace && tmux new-session -d -s meta_quest \
